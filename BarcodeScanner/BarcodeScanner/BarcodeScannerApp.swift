226f87b19fdc814a81f6a7ebb53aa8eb077aca05
Norbert Agoston <norbert.agst@gmail.com>
2024-02-15 10:51:42 +0200

    Basic project setup and UI layout
    - create new Barcode Scanner project
    - create basic UI layout

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Norbert Agoston on 15.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
