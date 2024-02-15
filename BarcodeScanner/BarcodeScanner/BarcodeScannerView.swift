754404841a9098782326f38de43ec023c7f6791f
Norbert Agoston <norbert.agst@gmail.com>
2024-02-15 12:54:55 +0200

    Handle scanned code
    - handle scanned code from ViewController to Coordinator to SwiftUI

--- BarcodeScanner/BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerView.swift
@@ -8,18 +8,20 @@
 import SwiftUI
 
 struct BarcodeScannerView: View {
+    @State private var scannedCode = ""
+    
     var body: some View {
         NavigationView {
             VStack {
-                Rectangle()
+                ScannerView(scannedCode: $scannedCode)
                     .frame(maxWidth: .infinity, maxHeight: 300)
                 Spacer().frame(height: 60)
                 Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
                     .font(.title)
-                Text("Not yet scaned")
+                Text(scannedCode.isEmpty ? "Not yet scaned" : scannedCode)
                     .bold()
                     .font(.largeTitle)
-                    .foregroundColor(.green)
+                    .foregroundColor(scannedCode.isEmpty ? .red : .green)
                     .padding()
             }
             .navigationTitle("Barcode Scanner")
