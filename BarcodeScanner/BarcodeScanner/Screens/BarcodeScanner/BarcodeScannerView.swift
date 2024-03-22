b686d9f5922f62bbbc22a441875f0716a51fae28
Norbert Agoston <norbert.agst@gmail.com>
2024-03-22 18:20:17 +0200

    Small code updates
    - simplify code

--- BarcodeScanner/BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
@@ -16,7 +16,7 @@ struct BarcodeScannerView: View {
             VStack {
                 ScannerView(scannedCode: $viewModel.scannedCode,
                             alertItem: $viewModel.alertItem)
-                    .frame(maxWidth: .infinity, maxHeight: 300)
+                    .frame(maxHeight: 300)
                 Spacer().frame(height: 60)
                 Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
                     .font(.title)
@@ -28,8 +28,8 @@ struct BarcodeScannerView: View {
             }
             .navigationTitle("Barcode Scanner")
             .alert(item: $viewModel.alertItem) { alertItem in
-                Alert(title: Text(alertItem.title),
-                      message: Text(alertItem.message),
+                Alert(title: alertItem.title,
+                      message: alertItem.message ,
                       dismissButton: alertItem.dismissButton
                       
                 )
--- BarcodeScanner/BarcodeScanner/Utilities/Alert.swift
+++ BarcodeScanner/BarcodeScanner/Utilities/Alert.swift
@@ -9,17 +9,17 @@ import SwiftUI
 
 struct AlertItem: Identifiable {
     let id = UUID()
-    let title: String
-    let message: String
+    let title: Text
+    let message: Text
     let dismissButton: Alert.Button
 }
 
 struct AlertContext {
-    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input",
-                                              message: "Something is wrong with the camera.",
+    static let invalidDeviceInput = AlertItem(title: Text("Invalid Device Input"),
+                                              message: Text("Something is wrong with the camera."),
                                               dismissButton: .default(Text("Ok")))
     
-    static let invalidScannedType = AlertItem(title: "Invalid Scan Type ",
-                                              message: "Scanned value is not valid.",
+    static let invalidScannedType = AlertItem(title: Text("Invalid Scan Type "),
+                                              message: Text("Scanned value is not valid."),
                                               dismissButton: .default(Text("Ok")))
 }
